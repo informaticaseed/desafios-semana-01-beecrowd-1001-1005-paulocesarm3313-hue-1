f172df8c252268bc26dde5c8cfbf3b1b7c62523b
paulocesarm3313-hue <paulocesarm3313@gmail.com>
2026-02-27 10:44:57 -0300

    Update desafio_1001.py

--- desafio_1001.py
+++ desafio_1001.py
@@ -1,14 +1,6 @@
-"""
-Beecrowd 1001 - Extremamente Básico
+A = int(input())
+B = int(input())
 
-Leia 2 valores inteiros e armazene-os nas variáveis A e B.
-Efetue a soma de A e B atribuindo o seu resultado na variável X.
-Imprima X conforme exemplo apresentado abaixo.
-Não apresente mensagem alguma além daquilo que está sendo especificado e
-não esqueça de imprimir o fim de linha após o resultado, caso contrário,
-você receberá "Presentation Error".
-"""
+X = A + B
 
-# Link do problema: https://judge.beecrowd.com/pt/problems/view/1001
-
-# Escreva sua solução abaixo
+print(f"X = {X}")
